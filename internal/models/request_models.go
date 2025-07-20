4b2b3eddc50e3825908dba0f9f135a9063857fca
faanross <moi@faanross.com>
2025-07-20 15:09:37 -0400

    agent side request package models

---
+++ internal/models/request_models.go
@@ -0,0 +1,67 @@
+package models
+
+// DNSRequest will hold the complete agent-side
+// configuration parsed from configs/request.yaml
+// It embeds 3 other structs, defined below
+type DNSRequest struct {
+	Header   Header   `yaml:"header"`
+	Question Question `yaml:"question"`
+	Resolver Resolver `yaml:"resolver"`
+}
+
+// Header represents the DNS header section.
+type Header struct {
+	// Query ID (16 bits): A random ID to match requests with replies.
+	ID uint16 `yaml:"id"`
+
+	// QR (1 bit): false means query, true means response
+	QR bool `yaml:"qr"`
+
+	// OpCode (4 bits): Specifies the kind of query.
+	OpCode string `yaml:"opcode"`
+
+	// Flags (1 bit each): These boolean flags control the behavior of the DNS query.
+	Authoritative      bool `yaml:"authoritative"`       // AA
+	Truncated          bool `yaml:"truncated"`           // TC
+	RecursionDesired   bool `yaml:"recursion_desired"`   // RD
+	RecursionAvailable bool `yaml:"recursion_available"` // RA
+
+	// Z (3 bits): Reserved bits. Per RFC 1035, this "must be zero".
+	// We expose it to allow for non-standard values (inspired by DNS sandwich).
+	Z uint8 `yaml:"z"`
+
+	// RCode (4 bits): Response code. We use uint8 (0-15) to allow setting
+	// any value, including standard codes and reserved ones (11 - 15).
+	RCode uint8 `yaml:"rcode"`
+
+	// Counts (16 bits each): These fields specify the number of entries in the
+	// subsequent sections of the DNS message. For a standard query, only
+	// QuestionCount will be 1. The others will be 0.
+	QuestionCount   uint16 `yaml:"question_count"`
+	AnswerCount     uint16 `yaml:"answer_count"`
+	AuthorityCount  uint16 `yaml:"ns_count"`
+	AdditionalCount uint16 `yaml:"additional_count"`
+}
+
+// Question represents the question section of a DNS query.
+type Question struct {
+	// Name: The domain name being queried (e.g., "www.vuilhond.com").
+	Name string `yaml:"name"`
+
+	// Type: The type of record being requested (e.g., "A", "AAAA", "MX").
+	Type string `yaml:"type"`
+
+	// Class: The protocol class, almost always "IN" for internet.
+	Class string `yaml:"class"`
+}
+
+// Resolver holds the information about the DNS resolver we're sending the packet to.
+type Resolver struct {
+	// UseSystemDefaults, if true, will ignore the IP and Port fields and instead
+	// discover and use the host operating system's default DNS resolver.
+	UseSystemDefaults bool `yaml:"use_system_defaults"`
+
+	// if UseSystemDefaults is false we can manually set the server/resolver here
+	IP   string `yaml:"ip"`
+	Port int    `yaml:"port"`
+}
