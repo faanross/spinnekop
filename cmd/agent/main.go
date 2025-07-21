f6b2b168e2de5df07ba9cba8794fa377cb7b42bd
faanross <moi@faanross.com>
2025-07-21 14:41:45 -0400

    create dns agent main base

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// assume go run from root, otherwise change path
+var pathToYamlFile = "./configs/agent.yaml"
+
+func main() {
+
+	// read yaml-file from disk
+
+	yamlFile, err := os.ReadFile(pathToYamlFile)
+	if err != nil {
+		fmt.Printf("Error reading YAML file: %v\n", err)
+		return
+	}
+
+}
